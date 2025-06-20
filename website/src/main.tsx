d15fe76c415e7c0e0501be9b74cd43ab2c220f58
Trương Dương Minh Nhật <tdminhnhat13092003@gmail.com>
2025-06-21 00:59:58 +0700

    website: import libs

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -4,6 +4,10 @@ import {RouterProvider} from "react-router";
 import router from "./routers/routers.tsx";
 import {Provider} from "react-redux";
 import {store} from "./stores/store.ts";
+import "./styles/main.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../node_modules/jquery/dist/jquery.js";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
