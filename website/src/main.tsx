6aa573aec61da379ad3e48bf3db78e98ddd937bd
Trương Dương Minh Nhật <tdminhnhat13092003@gmail.com>
2025-06-21 00:35:30 +0700

    website: config store

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {RouterProvider} from "react-router";
 import router from "./routers/routers.tsx";
+import {Provider} from "react-redux";
+import {store} from "./stores/store.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
