7ac568e5e3645da2eb17928145acdef3543f01ce
Trương Dương Minh Nhật <tdminhnhat13092003@gmail.com>
2025-06-21 01:01:30 +0700

    website: move file

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -4,7 +4,7 @@ import {RouterProvider} from "react-router";
 import router from "./routers/routers.tsx";
 import {Provider} from "react-redux";
 import {store} from "./stores/store.ts";
-import "./styles/main.scss";
+import "./styles/scss/main.scss";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.js";
 import "../node_modules/jquery/dist/jquery.js";
